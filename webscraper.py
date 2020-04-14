34c3eedeedac307a3b7cd8e87689ea9522bd1e63
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2020-04-14 13:39:57 -0400

    wroking scrape

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,4 @@
-# WEB SCRAPER 101
+# WEB SCRAPER
 
 import requests
 import csv
@@ -11,8 +11,8 @@ quote_number = 0
 json_content = True
 
 
-with open('scrape.csv', 'w') as csvfile:
-    columns = ['author', 'type', 'quote']
+with open("scrape.csv", "w") as csvfile:
+    columns = ["author", "tag", "quote"]
     writer = csv.DictWriter(csvfile, fieldnames = columns)
 
     writer.writeheader()
@@ -20,36 +20,20 @@ with open('scrape.csv', 'w') as csvfile:
         page_number += 1
         content = requests.get('http://quotes.toscrape.com/api/quotes?page={}'.format(page_number)).json()
         content = content["quotes"]
-        print(content)
-        json_content = False
+        print(content)               
 
-                            
-
-        #if content != []:
-            #for quotes in content:
-                #quote_number += 1
-                #print(quote_number)
-                #author = quotes["author"]["name"].encode("utf-8")
-                #try:
-                    #type = quotes["tags"][0].encode("utf-8")
-                #except:
-                    #type = "Unspecified"
-
-
-
-
-
-
-
-                            
-                    
-                                    
-
-                    
-
-
-
-
-
-
-
+        if content != []:
+            for quotes in content:
+                quote_number += 1
+                print(quote_number)
+                author = quotes["author"]["name"].encode("utf-8")
+                try:
+                    tag = quotes["tags"][0].encode("utf-8")
+                except:
+                    tag = "Unspecified"
+                quote = quotes["text"].encode("utf-8")
+                di = {"author" : author, "tag" : tag, "quote" : quote}
+                writer.writerow(di)
+        else:
+            json_content = False
+            print("Scrape complete!")
