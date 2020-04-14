b49c2eee53f9dbeba63f8bf126c3639eae583b63
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2020-04-14 11:22:51 -0400

    try/except block

--- webscraper.py
+++ webscraper.py
@@ -6,18 +6,44 @@ import json
 
 
 page_number = 0
+quote_number = 0
 
 json_content == True
 
 
 with open('scrape.csv', 'w') as csvfile:
-    columns = ['Author', 'Type', 'Quote']
+    columns = ['author', 'type', 'quote']
     writer = csv.DictWriter(csvfile, fieldnames = columns)
 
     writer.writeheader()
     while json_content == True:
         page_number += 1
-        content = requests.get('http://quotes
+        content = requests.get('http://quotes.toscrape.com.api.quotes?page={}'.format(page_number)).json()
+        content = content["quotes"]
+        print(page_number)
+
+        if content != []:
+            for quotes in content:
+                quote_number += 1
+                print(quote_number)
+                author = quotes["author"]["name"].encode("utf-8")
+                try:
+                    type = quotes["tags"][0].encode("utf-8")
+                except:
+
+
+
+
+
+                            
+                    
+                                    
+
+                    
+
+
+
+
 
 
 
