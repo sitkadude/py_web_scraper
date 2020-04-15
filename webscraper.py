04d5d0565eaad1fc9479d05eddb24c775f60d30a
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2020-04-15 09:28:49 -0400

    added comments

--- webscraper.py
+++ webscraper.py
@@ -1,27 +1,30 @@
 # WEB SCRAPER
 
+# DEPENDENCIES
 import requests
 import csv
 import json
 
 
 page_number = 0
-
 json_content = True
 
-
+# CREATE FILE OBJECT
 with open("scrape.csv", "w", encoding="utf8") as csvfile:
+    # CREATE ARRAY TO ASSIGN TO fieldnames PARAMETER
     columns = ["author", "tag", "quote"]
+    # CREATE OBJECT THAT MAPS DICTIONARIES ONTO OUTPUT ROWS
     writer = csv.DictWriter(csvfile, fieldnames = columns)
 
+    # WRITE ROW WITH fieldnames VALUES
     writer.writeheader()
+    # WHILE LOOP TO BEGIN CONDITIONAL
     while json_content == True:
         page_number += 1
-        content = requests.get('http://quotes.toscrape.com/api/quotes?page={}'.format(page_number)).json()
-        content = content["quotes"]
-        print(page_number)
-        quote_number = 0
+        # CREATE RESPONSE OBJECT
+        content = requests.get('http://quotes.toscrape.com/api/quotes?page={}'.format(page_number)).json() content = content["quotes"] print(page_number) quote_number = 0
 
+        # IF content DOES NOT EQUAL AN EMPTY LIST
         if content != []:
             for quotes in content:
                 quote_number += 1
